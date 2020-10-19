6feac5d651e17f61999d88d7c7eed7ed330d7a22
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2020-10-19 17:00:11 +0545

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://santos_parajuli:sasi987@nodejs.uhjwr.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret"
+  mongoURI: <Your MOGODB uri>,  // its like "mongodb+srv://<DatabaseUsername>:<Password>@nodejs.uhjwr.mongodb.net/<database>?retryWrites=true&w=majority"
+  secretOrKey: <Your secret key>
 };
